cb44905d9b089920ca3f14f98d997470fde8d8ea
ronynsc5 <ronynsc5@gmail.com>
2026-05-25 21:09:10 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// MyMind Pro - Supabase config
-// Preencha depois de criar seu projeto no Supabase.
-// O anon public key pode ficar no frontend, mas use RLS nas tabelas.
-window.MYMIND_SUPABASE_URL = 'https://pxigvmqxcqjgynqqelix.supabase.co';
-window.MYMIND_SUPABASE_ANON_KEY = 'sb_publishable_-2sY4tG_sRADyqPuaA-l2g_bw3W6EsS';
